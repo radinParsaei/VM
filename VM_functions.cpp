de7ad31304bddc08e822b585ec293699d73b1981
radinParaei <radinparsaei@gmail.com>
2020-03-18 19:02:37 +0330

    LoadLibrary -> LoadLibraryA

--- VM_functions.cpp
+++ VM_functions.cpp
@@ -11,14 +11,7 @@ extern "C" void DLCALL_task(){
   HINSTANCE hinstLib;
   dlfunc fn;
   std::string s = std::get<std::string>(pop());
-  int len;
-  int slength = (int)s.length() + 1;
-  len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
-  wchar_t* buf = new wchar_t[len];
-  MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
-  std::wstring r(buf);
-  delete[] buf;
-  hinstLib = LoadLibrary(r.c_str());
+  hinstLib = LoadLibrary(s.c_str());
   if(hinstLib != NULL){
     fn = (dlfunc) GetProcAddress(hinstLib, (LPCSTR)std::get<std::string>(pop()).c_str());
     if (NULL != fn){
